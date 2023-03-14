df1db832c3522e79b019aa3ffc82d26204786d3b
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-14 21:56:14 +0400

    Add stutuses and is_waiting()

--- sheets/classes.py
+++ sheets/classes.py
@@ -1,4 +1,4 @@
-from datetime import datetime, date
+from datetime import datetime
 from typing import NamedTuple
 
 
@@ -7,19 +7,45 @@ class Post(NamedTuple):
     text: str
     img_url: str
     vk_publish_at: datetime
+    vk_status: str
     tg_publish_at: datetime
+    tg_status: str
     ok_publish_at: datetime
+    ok_status: str
 
     @staticmethod
     def parse_row(input_list: list | tuple):
-        vk_publish_datetime = ' - '.join([input_list[3], input_list[4]])
-        tg_publish_datetime = ' - '.join([input_list[5], input_list[6]])
-        ok_publish_datetime = ' - '.join([input_list[7], input_list[8]])
+        def strptime(datetime_raw: str):
+            return datetime.strptime(datetime_raw, '%d.%m.%Y - %H:%M:%S')
+
+        def define_status(input_status: str):
+            if not input_status:
+                return 'waiting'
+            elif input_status == 'posted':
+                return 'posted'
+            else:
+                return 'error'
+
+        vk_publish_datetime = ' - '.join([input_list[4], input_list[5]])
+        tg_publish_datetime = ' - '.join([input_list[7], input_list[8]])
+        ok_publish_datetime = ' - '.join([input_list[10], input_list[11]])
         return Post(
             title=input_list[0],
             text=input_list[1],
             img_url=input_list[2],
-            vk_publish_at=datetime.strptime(vk_publish_datetime, '%d.%m.%Y - %H:%M:%S'),
-            tg_publish_at=datetime.strptime(tg_publish_datetime, '%d.%m.%Y - %H:%M:%S'),
-            ok_publish_at=datetime.strptime(ok_publish_datetime, '%d.%m.%Y - %H:%M:%S')
+            vk_publish_at=strptime(vk_publish_datetime),
+            vk_status=define_status(input_list[3]),
+            tg_publish_at=strptime(tg_publish_datetime),
+            tg_status=define_status(input_list[6]),
+            ok_publish_at=strptime(ok_publish_datetime),
+            ok_status=define_status(input_list[9])
+        )
+
+    def is_waiting(self) -> bool:
+        return any(
+            [
+                self.vk_status == 'waiting',
+                self.tg_status == 'waiting',
+                self.ok_status == 'waiting'
+            ]
         )
