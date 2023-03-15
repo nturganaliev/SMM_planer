a0308588317f7416eef7bd5a8dce3c60bf885f6b
nstonic <nstonic@gmail.com>
2023-03-15 17:47:55 +0400

    Optimize arguments, add set_post_statuses and renew_dashboard

--- sheets/methods.py
+++ sheets/methods.py
@@ -1,18 +1,23 @@
-from pprint import pprint
+import os
 from typing import Iterator
 
 import httplib2
 import apiclient
+from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
 
 from errors import retry_on_network_error
 from .classes import Post
 
+load_dotenv()
+_credentials_file = os.environ['GOOGLE_SHEETS_CREDENTIALS_FILE']
+_spreadsheet_id = os.environ['GOOGLE_SHEETS_SPREADSHEET_ID']
+
 
 @retry_on_network_error
-def login(credentials_file: str):
+def login():
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        credentials_file,
+        _credentials_file,
         ['https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive'])
     httpAuth = credentials.authorize(httplib2.Http())
@@ -20,10 +25,10 @@ def login(credentials_file: str):
 
 
 @retry_on_network_error
-def get_waiting_posts(credentials_file: str, spreadsheet_id: str) -> Iterator[Post]:
-    service = login(credentials_file)
+def get_waiting_posts() -> Iterator[Post]:
+    service = login(_credentials_file)
     plan_table = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheet_id,
+        spreadsheetId=_spreadsheet_id,
         range='Plan!A3:L10000',
         majorDimension='ROWS'
     ).execute()
@@ -31,11 +36,23 @@ def get_waiting_posts(credentials_file: str, spreadsheet_id: str) -> Iterator[Po
     return filter(lambda post: post.is_waiting(), all_posts)
 
 
-def get_all_posts(posts_rows):
+def get_all_posts(posts_rows: list[list]) -> Iterator[Post]:
     for post in posts_rows:
         try:
             post = Post.parse_row(post)
-        except (IndexError, ValueError):
-            continue
+        except (IndexError, ValueError) as ex:
+            print(ex)
         else:
             yield post
+
+
+@retry_on_network_error
+def set_post_statuses(statuses: dict):
+    service = login()
+    pass
+
+
+@retry_on_network_error
+def renew_dashboard():
+    service = login()
+    pass
--- start.py
+++ start.py
@@ -1,17 +1,13 @@
-import os
 import time
 from datetime import datetime
 from pprint import pprint
 from typing import Literal, Iterator
 
-from dotenv import load_dotenv
-
 from sheets.classes import Post
-from sheets.methods import get_waiting_posts
+from sheets.methods import get_waiting_posts, set_post_statuses, renew_dashboard
 
 
 def post_on_service(post: Post, service: Literal['vk', 'tg', 'ok']):
-    #  Постим на нужных сервисах и отправляем результат в Google Sheets
     if service == 'vk':
         print('VK')
         pprint(post)
@@ -26,23 +22,21 @@ def post_on_service(post: Post, service: Literal['vk', 'tg', 'ok']):
 
 
 def post_by_status(posts: Iterator[Post]):
+    statuses = {}
     for post in posts:
         if post.vk_status == 'waiting' and post.vk_publish_at <= datetime.now():
-            post_on_service(post, service='vk')
+            statuses['vk_posting_status'] = post_on_service(post, service='vk')
         if post.tg_status == 'waiting' and post.tg_publish_at <= datetime.now():
-            post_on_service(post, service='tg')
+            statuses['tg_posting_status'] = post_on_service(post, service='tg')
         if post.ok_status == 'waiting' and post.ok_publish_at <= datetime.now():
-            post_on_service(post, service='ok')
+            statuses['ok_posting_status'] = post_on_service(post, service='ok')
+    set_post_statuses(statuses)
+    renew_dashboard()
 
 
 def main():
-    load_dotenv()
     while True:
-        # Читаем гугл табличку и запускаем постинг, если надо. В табличке отмечаем сделанное
-        posts = get_waiting_posts(
-            credentials_file=os.environ['GOOGLE_SHEETS_CREDENTIALS_FILE'],
-            spreadsheet_id=os.environ['GOOGLE_SHEETS_SPREADSHEET_ID']
-        )
+        posts = get_waiting_posts()
         post_by_status(posts)
         time.sleep(3)
 
