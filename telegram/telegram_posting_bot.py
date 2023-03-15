930a65342b9e80b5486d0719e46a92a771fa6596
ZHartem21 <zhartem21@gmail.com>
2023-03-15 13:34:15 +0300

    Create telegram posting bot

---
+++ telegram/telegram_posting_bot.py
@@ -0,0 +1,24 @@
+import os
+
+import telegram
+from dotenv import load_dotenv
+
+
+def create_post(post_text, post_image=False, telegram_channel=None):
+    load_dotenv('access_tokens.env')
+    if not telegram_channel:
+        telegram_channel = os.environ['TELEGRAM_CHANNEL']
+    telegram_bot_token = os.environ['TELEGRAM_TOKEN']
+    telegram_bot = telegram.Bot(token=telegram_bot_token)
+    if post_image:
+        with open(os.path.join('images', post_image), 'rb') as image:
+            telegram_bot.send_photo(
+                chat_id=telegram_channel,
+                photo=image,
+                caption=post_text
+            )
+    else:
+        telegram_bot.send_message(
+            chat_id=telegram_channel,
+            text=post_text
+        )
