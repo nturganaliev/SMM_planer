730844a003e5226426b5b4bbcb3d97958a0b283d
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-16 23:40:59 +0400

    Delete path.join

--- vk/vk_posting_bot.py
+++ vk/vk_posting_bot.py
@@ -17,7 +17,7 @@ def create_post(post_text, post_image=None, group_id=None):
         vk.wall.post(message=post_text, owner_id=f'-{group_id}')
         return True
     upload = vk_api.VkUpload(vk_session)
-    with open(os.path.join('images', post_image), 'rb') as image:
+    with open(post_image, 'rb') as image:
         uploaded_image = upload.photo_wall(image, user_id=group_id, caption=post_text)
         image_for_post = f'photo{uploaded_image[0]["owner_id"]}_{uploaded_image[0]["id"]}'
         vk.wall.post(message=post_text, owner_id=f'-{group_id}', attachments=image_for_post, from_group=1)
