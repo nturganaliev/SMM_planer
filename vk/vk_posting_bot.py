c2eb32dee6a767d8c3a9ae53475f5b8766b89198
ZHartem21 <zhartem21@gmail.com>
2023-03-16 21:13:46 +0300

    Update vk_posting_bot.py

--- vk/vk_posting_bot.py
+++ vk/vk_posting_bot.py
@@ -9,14 +9,8 @@ def create_post(post_text, post_image=None, group_id=None):
     if not group_id:
         group_id = os.environ['VK_GROUP_ID']
 
-    vk_login = os.environ['VK_LOGIN']
-    vk_password = os.environ['VK_PASSWORD']
-    vk_session = vk_api.VkApi(vk_login, vk_password)
-    try:
-        vk_session.auth()
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return False
+    vk_token = os.environ['VK_ACCESS_TOKEN']
+    vk_session = vk_api.VkApi(token=vk_token)
 
     vk = vk_session.get_api()
     if not post_image:
@@ -26,5 +20,8 @@ def create_post(post_text, post_image=None, group_id=None):
     with open(os.path.join('images', post_image), 'rb') as image:
         uploaded_image = upload.photo_wall(image, user_id=group_id, caption=post_text)
         image_for_post = f'photo{uploaded_image[0]["owner_id"]}_{uploaded_image[0]["id"]}'
-        vk.wall.post(message=post_text, owner_id=f'-{group_id}', attachments=image_for_post)
+        vk.wall.post(message=post_text, owner_id=f'-{group_id}', attachments=image_for_post, from_group=1)
         return True
+
+
+create_post('Text')
